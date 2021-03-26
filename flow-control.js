4faa3b46d81002daa4d00c94b7cc836681cc2bd4
stellar-function-e8f8 <kwooten1993@gmail.com>
2021-03-26 00:16:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,12 @@
 function basicTeenager(age) {
 if (age >= 13 && age <= 19) {
-  console.log("You are a teenager!")
+  return "You are a teenager!"
 }
 }
 
 function teenager(age) {
   if (age >= 13 && age <= 19 ){
-    console.log("You are a teenager!")
+    return "You are a teenager!"
   }
 else if {
   return "You are not a teenager"
